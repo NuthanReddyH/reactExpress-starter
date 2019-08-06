9a2e53739270c51dc57a1848e3f04b1532610107
nuthanreddyH <nuthanreddy1194@gmail.com>
2019-08-06 15:44:56 +0530

    react express

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/api/customers', (req,res) => {
+    const customers = [
+        {
+            id: 1,
+            firstName: "John",
+            lastName: "Doe"
+        },
+        {
+            id: 2,
+            firstName: "Steve",
+            lastName: "Smith"
+        },
+        {
+            id: 3,
+            firstName: "Dave",
+            lastName: "Warner"
+        }
+    ];
+    res.json(customers);
+})
+const port = 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
